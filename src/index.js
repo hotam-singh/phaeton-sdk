36cf20c04151db019c4f47b07f260a7887a1503b
hotam-singh <hotam.hbti123@gmail.com>
2019-10-14 13:50:06 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2019 Phaeton Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Phaeton Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const BigNum = require('@phaetonhq/bignum');
+const cryptography = require('@phaetonhq/phaeton-cryptography');
+const transactions = require('@phaetonhq/phaeton-transactions');
+const {
+	Application,
+	version,
+	systemDirs,
+	configurator,
+} = require('phaeton-framework');
+
+const samples = require('./samples');
+
+module.exports = {
+	Application,
+	version,
+	systemDirs,
+	configurator,
+	BigNum,
+	cryptography,
+	transactions,
+	...samples,
+};
